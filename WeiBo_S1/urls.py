013d36ae80271052fdeda20327af6a9af9e2f3fc
ansheng <anshengme.com@gmail.com>
2016-09-23 17:30:53 +0800

    a

--- WeiBo_S1/urls.py
+++ WeiBo_S1/urls.py
@@ -19,7 +19,9 @@ from apps import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', views.login),
     url(r'^$', views.index),
     # 用户个人主页，接受一个UID
+
     url(r'^u/[\w+]', views.userprofile)
 ]
