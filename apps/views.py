b4c6ac1497397a174943370e1a9dc0d537d1fcfc
ansheng <anshengme.com@gmail.com>
2016-09-23 18:45:36 +0800

    2016-09-23 18:45:35

--- apps/views.py
+++ apps/views.py
@@ -17,9 +17,14 @@ def userprofile(request):
     # 获取用户传入过来的用户名
     CurrentUserName = request.path.split('/')[2]
     Code = len(User.objects.filter(username=CurrentUserName))
-
+    # 没有获取到此用户返回404
     if Code != 1:
         return HttpResponse('404')
+    # 获取前端所需要的用户信息
+    UserProfile = list(
+        models.UserProfile.objects.filter(user__username='ansheng').values('name', 'sex', 'tags', 'head_img',
+
+                                                                           'follow_list'))[0]
     UserData = {
         'UserName': 'HelloWorld',
         'Brief': '我是签名我是签名我是签名我是签名我是签名我是签名',
